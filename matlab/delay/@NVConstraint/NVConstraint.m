6cdf188d4feaf8c90a82028234c22574fd80c493
Jonas Otten <jonas.otten@rub.de>
2017-05-08 14:37:54 +0000

    Kommentare ergänzt und Fallunterscheidungen, die Ausgabe nur bei Aufruf durch Nutzer zulassen, hinzugefügt

--- matlab/delay/@NVConstraint/NVConstraint.m
+++ matlab/delay/@NVConstraint/NVConstraint.m
@@ -1,6 +1,6 @@
 classdef NVConstraint < EqualityConstraint
-    %NVCONSTRAINT Summary of this class goes here
-    %   Detailed explanation goes here
+    %NVCONSTRAINT the instances of this class are objects representing
+    %normal vector constraints. The procedures initialize those constraints
     
     properties
         
@@ -10,23 +10,25 @@ classdef NVConstraint < EqualityConstraint
         eqNVSys % EqualityConstraint object
         eqConnect % EqualityConstraint object
         
-        nVarAugSys
-        nVarNVSys
+        nVarAugSys % number of variables in augemented system
+        nVarNVSys % number of variables in normal vector system
         
         inequalities % function handle
-        optionsEqConsInit
-        optionsInitOptim
+        optionsEqConsInit % options for numerical solver
+        optionsInitOptim % options for auxiliary optimization
         
     end
     
     
     properties(SetAccess=protected)
-        inequalityIndex
+        inequalityIndex % index of the inequality constraint of this NVConstraint within all constaints
+        problemDDE % differential equation for calculation of stability
+        numMinEig % numerical constant for the ODE approximation of the DDE
     end
     
     methods
-        function aNVCon = NVConstraint(aDDENLP,type,augSysHandle,nVSysHandle,nVvars)
-           
+        function aNVCon = NVConstraint(aDDENLP,type,augSysHandle,nVSysHandle,nVvars) % constructor
+            
             %% translate human readable input format to solver readable format
             switch type
                 case 'fold'
@@ -64,7 +66,7 @@ classdef NVConstraint < EqualityConstraint
                 nEqAugSys+nEqNVSys+aDDENLP.vars.nominal.alpha.nVar,...
                 nVvars,...
                 aDDENLP.occupiedEqs);
-            
+
             %% define solver options
             aNVCon.optionsEqConsInit= aDDENLP.optionsInitEqCons;
             aNVCon.optionsInitOptim = aDDENLP.optionsInitOptim;
@@ -77,14 +79,18 @@ classdef NVConstraint < EqualityConstraint
             aNVCon.eqAugSys=EqualityConstraint(augSysHandle,nEqAugSys,nVvars,aDDENLP.occupiedEqs);
             aNVCon.eqNVSys=EqualityConstraint(nVSysHandle,nEqNVSys,nVvars,aDDENLP.occupiedEqs+nEqAugSys);
             aNVCon.eqConnect=EqualityConstraint(connectionConstraint,nVvars.alpha.nVar,nVvars.nVar,aDDENLP.occupiedEqs+nEqAugSys+nEqNVSys);
-             
+            %             aNVCon.eqConnect=EqualityConstraint(connectionConstraint,1,nVvars.nVar,aDDENLP.occupiedEqs+nEqAugSys+nEqNVSys);
+            
             
             aNVCon.inequalities = @(y)aDDENLP.minDist*sqrt(aDDENLP.nAlpha)-y(aNVCon.vars.l.index);
             
             aNVCon.nVarAugSys=nVvars.x.nVar+nVvars.alpha.nVar+nVvars.omega.nVar+nVvars.w1.nVar+nVvars.w2.nVar;
             aNVCon.nVarNVSys=nVvars.v1.nVar+nVvars.v2.nVar+nVvars.g1.nVar+nVvars.g2.nVar+nVvars.u.nVar+nVvars.r.nVar;
-                       
+            
             aNVCon.inequalityIndex=aDDENLP.occupiedIneqs+1;
+            
+            aNVCon.problemDDE = aDDENLP.problemDDE;
+            aNVCon.numMinEig = aDDENLP.numMinEig;
             
         end
         
@@ -93,11 +99,11 @@ classdef NVConstraint < EqualityConstraint
             
             % evaluate number of leading variables, that are not part of this
             % constraint
-            offset=aNVCon.vars.x.index(1)-1;            
-                        
+            offset=aNVCon.vars.x.index(1)-1;
+            
             otherVariables=zeros(offset,1);
             otherVariables(aVarCollection.p.index)=aVarCollection.p.values;
-
+            
             rhs = @(y)aNVCon.eqAugSys.conFun([otherVariables;y]);
             
             if nargin>1
@@ -111,8 +117,8 @@ classdef NVConstraint < EqualityConstraint
                 x0 = ones(aNVCon.nVarAugSys,1);
             end
             
-            [x,~,exitflag] = fsolve(rhs,x0,aNVCon.optionsEqConsInit);
-                        
+            [x,res,exitflag] = fsolve(rhs,x0,aNVCon.optionsEqConsInit);
+            
             aNVCon.vars.x.values = x(aNVCon.vars.x.index-offset);
             aNVCon.vars.alpha.values = x(aNVCon.vars.alpha.index-offset);
             aNVCon.vars.omega.values = x(aNVCon.vars.omega.index-offset);
@@ -121,9 +127,14 @@ classdef NVConstraint < EqualityConstraint
             
             if exitflag > 0
                 aNVCon.status = 2;
-                fprintf('\nfound point on critical manifold, fsolve exitflag was %d\n', exitflag)
+                
+                callerFunction = dbstack;
+                if ~strcmp(callerFunction(3).name, 'DDENLP.moveAwayFromManifolds')
+                    fprintf('\nfound point on critical manifold, fsolve exitflag was %d\n', exitflag)
+                end
             else
                 warning('did not find critical point, fsolve exitflag was %d',exitflag)
+                disp(res);
             end
         end
         
@@ -136,7 +147,7 @@ classdef NVConstraint < EqualityConstraint
             
             offset = aNVCon.vars.x.index(1)-1;
             
-            otherVariables=zeros(offset,1);            
+            otherVariables=zeros(offset,1);
             otherVariables(aNVCon.vars.p.index)=aNVCon.vars.p.values;
             
             manifoldCon = @(y)deal([],aNVCon.eqAugSys.conFun([otherVariables;y]));
@@ -146,7 +157,7 @@ classdef NVConstraint < EqualityConstraint
                 aNVCon.vars.omega.values;...
                 aNVCon.vars.w1.values;...
                 aNVCon.vars.w2.values];
-
+            
             % perform actual search for closest critical point
             J=@(y)norm(alphaNom.values-y(aNVCon.vars.alpha.index-offset),2);
             [x,l,exitflag] = fmincon(J,x0,[],[],[],[],[],[],manifoldCon,aNVCon.optionsInitOptim);
@@ -158,18 +169,22 @@ classdef NVConstraint < EqualityConstraint
             aNVCon.vars.w1.values = x(aNVCon.vars.w1.index-offset);
             aNVCon.vars.w2.values = x(aNVCon.vars.w2.index-offset);
             aNVCon.vars.l.values = l;
-
+            
             % give feedback
             if exitflag>0
                 aNVCon.status=3;
-                fprintf('found closest point on critical manifold at distance l = %f, fsolve exitflag was %d\n',l,exitflag)
-              %  disp(aNVCon.vars.alpha.values')
+                callerFunction = dbstack;
+                if ~strcmp(callerFunction(3).name, 'DDENLP.moveAwayFromManifolds')
+                    fprintf('found closest point on critical manifold at distance l = %f, fmincon exitflag was %d\n',l,exitflag)
+                end
             else
-                 warning('did not find closest critical point, fmincon exitflag was %d', exitflag)
+                warning('did not find closest critical point, fmincon exitflag was %d', exitflag)
             end
         end
         
         function  findNormalVector(aNVCon,alphaNom)
+            
+            directionMode = 2;
             
             if aNVCon.status < 2
                 error('cannot search for normal vector without a critical point');
@@ -190,22 +205,69 @@ classdef NVConstraint < EqualityConstraint
             
             x0 = ones(aNVCon.nVarNVSys,1);
             
-            [x,~,exitflag] = fsolve(rhs,x0,aNVCon.optionsEqConsInit);
+            [x,res,exitflag] = fsolve(rhs,x0,aNVCon.optionsEqConsInit);
             
-            offset=aNVCon.vars.v1.index(1)-1;
-            
+            if ~strcmp(aNVCon.type,'fold')
+                offset=aNVCon.vars.v1.index(1)-1;
+            else
+                offset=aNVCon.vars.w1.index(end);
+            end
             %% THIS IS A TEMPORARY CODE SEGMENT WHICH ASSUMES THE NOMINAL POINT IS IN THE STABLE REGION
             % IT FLIPS THE NORMAL VECTOR ALLWAYS TOWARDS THE NOMINAL POINT. LATER THE NORMAL VECTOR HAS TO POINT TO THE STABLE REGION
             
-            r=x(aNVCon.vars.r.index-offset);
-            r=r/norm(r,2);
-            alphaNom=alphaNom.values;
-            alphaCrit=aNVCon.vars.alpha.values;
-            direction=sign((r'*r)\r'*(alphaNom-alphaCrit));
-            
+            switch directionMode
+                case 1      % FLIPS THE NORMAL VECTOR ALLWAYS TOWARDS THE NOMINAL POINT
+                    
+                    r = x(aNVCon.vars.r.index-offset);
+                    r = r/norm(r,2);
+                    alphaNom = alphaNom.values;
+                    alphaCrit = aNVCon.vars.alpha.values;
+                    direction = sign((r'*r)\r'*(alphaNom-alphaCrit));
+                    
+                case 2  % FLIPS THE NORMAL VECTOR ALLWAYS TOWARDS THE STABLE REGION, determined by eigenvalues
+                    
+                    shiftLength = 1e-3;
+                    
+                    r = x(aNVCon.vars.r.index-offset);
+                    r = r/norm(r,2);
+                    
+                    alpha1 = aNVCon.vars.alpha.values - shiftLength*r;                    
+                    alpha2 = aNVCon.vars.alpha.values + shiftLength*r;
+                    
+                    xStSt = aNVCon.vars.x.values;
+                    
+                    %% prepare stability calculations
+                    p=aNVCon.vars.p.values;
+                    
+                    myDDE = @(xx,alpha)aNVCon.problemDDE.rhs(xx(:,1),xx(:,2:end),alpha,p)';
+                    myNtau = aNVCon.problemDDE.ntau;
+                    myDelays = @(k,xx,alpha)(k==(1:myNtau))*aNVCon.problemDDE.delays(xx,alpha,p);
+                    
+                    funcs = set_funcs('sys_rhs',myDDE,...
+                        'sys_ntau',myNtau,...
+                        'sys_tau',myDelays);
+                    
+                    [lambda1,~,~] = checkStability(funcs,alpha1,xStSt,aNVCon.numMinEig);
+                    if isempty(lambda1)
+                        error('did not find an eigevalue with Re(lambda)>aDDENLP.numMinEig. Please chose a smaller value for aDDENLP.numMinEig')
+                    end
+                    
+                    [lambda2,~,~] = checkStability(funcs,alpha2,xStSt,aNVCon.numMinEig);
+                    if isempty(lambda2)
+                        error('did not find an eigevalue with Re(lambda)>aDDENLP.numMinEig. Please chose a smaller value for aDDENLP.numMinEig')
+                    end
+                    
+                    %% evaluate
+                    direction = sign(lambda1-lambda2);
+                    if direction == 0
+                        warning('could not determine which region is stable and which is unstable')
+                    end
+                    %                 case 3  % FLIPS THE NORMAL VECTOR ALLWAYS TOWARDS THE STABLE REGION, determined by analytical expression
+            end
             
             % END OF THE TEMPORARY SEGMENT
-
+            
+            
             aNVCon.vars.v1.values = direction*x(aNVCon.vars.v1.index-offset);
             aNVCon.vars.v2.values = direction*x(aNVCon.vars.v2.index-offset);
             aNVCon.vars.g1.values = direction*x(aNVCon.vars.g1.index-offset);
@@ -213,25 +275,19 @@ classdef NVConstraint < EqualityConstraint
             aNVCon.vars.u.values = direction*x(aNVCon.vars.u.index-offset);
             aNVCon.vars.r.values = direction*x(aNVCon.vars.r.index-offset);
             
-            
-            %             blaNV=rhs([aNVCon.vars.v1.values;...
-            %                 aNVCon.vars.v2.values;...
-            %                 aNVCon.vars.g1.values;...
-            %                 aNVCon.vars.g2.values;...
-            %                 aNVCon.vars.u.values;...
-            %                 aNVCon.vars.r.values;...
-            %                 aNVCon.vars.l.values])
-            %
-            
             if exitflag>0
                 if aNVCon.status == 3
                     aNVCon.status=4;
-                    fprintf('found normal vector on closest critical manifold point, fsolve exitflag was %d\n',exitflag)
+                    callerFunction = dbstack;
+                    if ~strcmp(callerFunction(3).name, 'DDENLP.moveAwayFromManifolds')
+                        fprintf('found normal vector on closest critical manifold point, fsolve exitflag was %d\n',exitflag)
+                    end
                 else
                     fprintf('found normal vector on a critical manifold point, fsolve exitflag was %d\n',exitflag)
                 end
-               
+                
             else
+                disp(res)
                 warning('did not find normal vector on critical point, fsolve exitflag was %d\n', exitflag)
             end
         end
@@ -260,27 +316,11 @@ classdef NVConstraint < EqualityConstraint
             otherVariables=zeros(offset,1);
             otherVariables(alphaNom.index)=alphaNom.values;
             otherVariables(aNVCon.vars.p.index)=aNVCon.vars.p.values;
-                       
-            rhs = @(y)aNVCon.conFun([otherVariables;y]);            
-% 
-%             l=aNVCon.vars.l.values
-%             
-%             bla=rhs(x0);
-%             connectionResBefore=bla(end-11:end)
-%             
-%             r=aNVCon.vars.r.values;
-%             r=r/norm(r,2);
-%             l=aNVCon.vars.l.values;
-%             alphaNom=alphaNom.values;
-%             alphaCrit=aNVCon.vars.alpha.values;
-%             
-%             checkBefore=[l*r,alphaNom-alphaCrit]
-%             
-%           [bla(end-11:end)/norm(bla(end-11:end),2),aNVCon.vars.r.values/norm(aNVCon.vars.r.values,2)]
-
             
-            [x,res,exitflag] = fsolve(rhs,x0,aNVCon.optionsEqConsInit);            
-                       
+            rhs = @(y)aNVCon.conFun([otherVariables;y]);
+            
+            [x,res,exitflag] = fsolve(rhs,x0,aNVCon.optionsEqConsInit);
+            
             aNVCon.vars.x.values = x(aNVCon.vars.x.index-offset);
             aNVCon.vars.alpha.values = x(aNVCon.vars.alpha.index-offset);
             aNVCon.vars.omega.values = x(aNVCon.vars.omega.index-offset);
@@ -293,10 +333,13 @@ classdef NVConstraint < EqualityConstraint
             aNVCon.vars.u.values = x(aNVCon.vars.u.index-offset);
             aNVCon.vars.r.values = x(aNVCon.vars.r.index-offset);
             aNVCon.vars.l.values = x(aNVCon.vars.l.index-offset);
-
+            
             if exitflag>0
                 aNVCon.status=5;
-                fprintf('found connection of nominal and critical point, fsolve exitflag was %d \n',exitflag)
+                callerFunction = dbstack;
+                if ~strcmp(callerFunction(3).name, 'DDENLP.moveAwayFromManifolds')
+                    fprintf('found connection of nominal and critical point, fsolve exitflag was %d \n\n',exitflag)
+                end
             else
                 disp(res)
                 warning('no connection found, fsolve exitflag was %d',exitflag)
