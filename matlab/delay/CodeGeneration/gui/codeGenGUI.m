92931ab4a48dbe777e0f0836476e2668c344ea5c
Paul Mertin <paul.mertin@rub.de>
2017-07-07 08:58:08 +0000

    gui update

--- matlab/delay/CodeGeneration/gui/codeGenGUI.m
+++ matlab/delay/CodeGeneration/gui/codeGenGUI.m
@@ -1,58 +1,58 @@
 %% Program to open input dialogs (gui), code generation, code manipulation and compiling
-% add folders gui, explicitAEs and codegen to matlab
+% add folders gui and codegen to matlab
 
 %% start gui
 
 systemInput;
 % systemInput_limit;
 
-% if additional equations are needed (eg:system equations have additional
-% parameter)
-explicitAEs;
-
 %% generate code for maple
 
 codeGeneration;
 %%
-clear aes alphavec ans answer anum del delnum fileID i j l tauname way x xdot xnames xnum
+clear aes alphavec ans answer del fileID i j l tauname way x xdot xnames maxrealpart
 
-system('maple codegen.txt');
+system_order = ['maple ' cg_name];
 
+system(system_order);
+
+clear p_name system_order
 %% code manipulation to make it compilable for matlab
 
-if y(1) % Fold
+if choiceMani(1) % Fold
     mexWrapperGen_Fold;
     mexWrapperGen_FoldNV;
 end
-if y(2) % modFold
+if choiceMani(2) % modFold
     mexWrapperGen_Modfold;
     mexWrapperGen_ModfoldNV;
 end
-if y(3) % Hopf
+if choiceMani(3) % Hopf
     mexWrapperGen_HopfMani;
     mexWrapperGen_HopfNV;
 end
-if y(4) % modHopf
+if choiceMani(4) % modHopf
     mexWrapperGen_ModHopf;
     mexWrapperGen_ModHopfNV;
 end
-
+mexWrapperGen_DDE;
 %% Compile for matlab
 
 % mex ... ;
-if y(1)
+if choiceMani(1)
     mex fold_Manifold.c
     mex fold_NV.c
 end
-if y(2)
+if choiceMani(2)
     mex modfold_Manifold.c;
     mex modfold_NV.c;
 end
-if y(3)
+if choiceMani(3)
     mex hopf_Manifold.c;
     mex hopf_NV.c;
 end
-if y(4)
+if choiceMani(4)
     mex modhopf_Manifold.c
     mex modhopf_NV.c
 end
+mex DDE_wrapped.c
