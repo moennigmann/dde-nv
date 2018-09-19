88cf9d95a774f03d5b66d50090eb8f6891d78daf
Jonas Otten <jonas.otten@rub.de>
2018-09-19 07:52:57 +0000

    p berücktsichtigt

--- matlab/delay/varCollection.m
+++ matlab/delay/varCollection.m
@@ -19,13 +19,20 @@
 %> @retval vars variables vector to return
 %======================================================================
 
-function [ vars ] = varCollection( type, offset, x, alpha, p)
+function [ vars ] = varCollection( type, offset, x, alpha, p, algVars)
 %VARCOLLETION creates variable collections for different manifold types
 
 
+if algVars
+    p = VariableVector(p.values,offset+x.nVar+alpha.nVar,p.names);
+    pnVar=p.nVar;
+else
+    pnVar = 0;
+end
+    
 vars.x=VariableVector(x.values,offset,x.names);
 vars.alpha=VariableVector(alpha.values,offset+x.nVar,alpha.names);
-vars.p=VariableVector(p.values,offset+x.nVar+alpha.nVar,p.names);
+vars.p=p;
 vars.omega=VariableVector([], offset+x.nVar,{});
 vars.w1=VariableVector([], offset+x.nVar,{});
 vars.w2=VariableVector([], offset+x.nVar,{});
@@ -33,6 +40,7 @@ vars.v1=VariableVector([], offset+x.nVar,{});
 vars.v2=VariableVector([], offset+x.nVar,{});
 vars.g1=VariableVector([], offset+x.nVar,{});
 vars.g2=VariableVector([], offset+x.nVar,{});
+vars.k=VariableVector([], offset+x.nVar,{});
 vars.u=VariableVector([], offset+x.nVar,{});
 vars.r=VariableVector([],offset+x.nVar,{});
 vars.l=VariableVector([],offset+x.nVar,{});
@@ -43,10 +51,10 @@ switch type
     case 'nominal'
         vars.nVar=x.nVar+alpha.nVar;    
     case 'fold'
-        vars.w1=VariableVector(ones(x.nVar,1),offset+x.nVar+alpha.nVar+p.nVar,{'w1'});
-        vars.r=VariableVector(zeros(alpha.nVar,1),offset+2*x.nVar+alpha.nVar+p.nVar,{'r'});
-        vars.l=VariableVector(zeros(1,1),offset+2*x.nVar+2*alpha.nVar+p.nVar,{'l'});
-        vars.nVar=2*x.nVar+2*alpha.nVar+p.nVar+1;
+        vars.w1=VariableVector(ones(x.nVar,1),offset+x.nVar+alpha.nVar+pnVar,{'w1'});
+        vars.r=VariableVector(zeros(alpha.nVar,1),offset+2*x.nVar+alpha.nVar+pnVar,{'r'});
+        vars.l=VariableVector(zeros(1,1),offset+2*x.nVar+2*alpha.nVar+pnVar,{'l'});
+        vars.nVar=2*x.nVar+2*alpha.nVar+pnVar+1;
 %     case 'modfold'
 %         vars.w1=VariableVector(ones(x.nVar,1),offset+x.nVar+alpha.nVar,{'w1'});
 %         vars.v1=VariableVector(zeros(x.nVar,1),offset+2*x.nVar+alpha.nVar,{'v1'});
@@ -58,27 +66,30 @@ switch type
 
 %HIER MODIFIZIERT UM AUCH DIE INDIZES DER FOLGENDEN VARIABLEN ANZUPASSEN
     case 'modfold'
-        vars.w1=VariableVector(ones(x.nVar,1),offset+x.nVar+alpha.nVar+p.nVar,{'w1'});
-        vars.v1=VariableVector(zeros(x.nVar,1),offset+2*x.nVar+alpha.nVar+p.nVar,{'v1'});
-        vars.g1=VariableVector(zeros(1,1),offset+3*x.nVar+alpha.nVar+p.nVar,{'g1'});
-        vars.u=VariableVector(zeros(x.nVar,1),offset+3*x.nVar+alpha.nVar+1+p.nVar,{'u'});
-        vars.r=VariableVector(zeros(alpha.nVar,1),offset+4*x.nVar+alpha.nVar+1+p.nVar,{'r'});
-        vars.l=VariableVector(zeros(1,1),offset+4*x.nVar+2*alpha.nVar+1+p.nVar,{'l'});
-        vars.nVar=4*x.nVar+2*alpha.nVar+2+p.nVar;
-%Bis hier!
+        vars.w1=VariableVector(ones(x.nVar,1),offset+x.nVar+alpha.nVar+pnVar,{'w1'});
+        vars.v1=VariableVector(zeros(x.nVar,1),offset+2*x.nVar+alpha.nVar+pnVar,{'v1'});
+        vars.g1=VariableVector(zeros(1,1),offset+3*x.nVar+alpha.nVar+pnVar,{'g1'});
+        vars.k=VariableVector(zeros(pnVar,1),offset+3*x.nVar+alpha.nVar+1+pnVar,{});
+        vars.u=VariableVector(zeros(x.nVar,1),offset+3*x.nVar+alpha.nVar+1+2*pnVar,{'u'});
+        vars.r=VariableVector(zeros(alpha.nVar,1),offset+4*x.nVar+alpha.nVar+1+2*pnVar,{'r'});
+        vars.l=VariableVector(zeros(1,1),offset+4*x.nVar+2*alpha.nVar+1+2*pnVar,{'l'});
+        vars.nVar=4*x.nVar+2*alpha.nVar+2+2*pnVar;
+
         
     case 'hopf'
-        vars.omega=VariableVector(zeros(1,1), offset+x.nVar+alpha.nVar+p.nVar,{'omega'});
-        vars.w1=VariableVector(ones(x.nVar,1), offset+x.nVar+alpha.nVar+p.nVar+1,{'w1'});
-        vars.w2=VariableVector(zeros(x.nVar,1), offset+2*x.nVar+alpha.nVar+p.nVar+1,{'w2'});
-        vars.v1=VariableVector(zeros(x.nVar,1), offset+3*x.nVar+alpha.nVar+p.nVar+1,{'v1'});
-        vars.v2=VariableVector(zeros(x.nVar,1), offset+4*x.nVar+alpha.nVar+p.nVar+1,{'v2'});
-        vars.g1=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+p.nVar+1,{'g1'});
-        vars.g2=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+p.nVar+2,{'g2'});
-        vars.u=VariableVector(zeros(x.nVar,1), offset+5*x.nVar+alpha.nVar+p.nVar+3,{'u'});
-        vars.r=VariableVector(zeros(alpha.nVar,1),offset+6*x.nVar+alpha.nVar+p.nVar+3,{'r'});
-        vars.l=VariableVector(zeros(1,1),offset+6*x.nVar+2*alpha.nVar+p.nVar+3,{'l'});
-        vars.nVar=6*x.nVar+2*alpha.nVar+p.nVar+4;
+        vars.omega=VariableVector(zeros(1,1), offset+x.nVar+alpha.nVar+pnVar,{'omega'});
+        vars.w1=VariableVector(ones(x.nVar,1), offset+x.nVar+alpha.nVar+pnVar+1,{'w1'});
+        vars.w2=VariableVector(zeros(x.nVar,1), offset+2*x.nVar+alpha.nVar+pnVar+1,{'w2'});
+        vars.v1=VariableVector(zeros(x.nVar,1), offset+3*x.nVar+alpha.nVar+pnVar+1,{'v1'});
+        vars.v2=VariableVector(zeros(x.nVar,1), offset+4*x.nVar+alpha.nVar+pnVar+1,{'v2'});
+        vars.g1=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+pnVar+1,{'g1'});
+        vars.g2=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+pnVar+2,{'g2'});
+        vars.k=VariableVector(zeros(pnVar,1), offset+5*x.nVar+alpha.nVar+pnVar+3,{'k'});
+        vars.u=VariableVector(zeros(x.nVar,1), offset+5*x.nVar+alpha.nVar+2*pnVar+3,{'u'});
+        vars.r=VariableVector(zeros(alpha.nVar,1),offset+6*x.nVar+alpha.nVar+2*pnVar+3,{'r'});
+        vars.l=VariableVector(zeros(1,1),offset+6*x.nVar+2*alpha.nVar+2*pnVar+3,{'l'});
+        vars.nVar=6*x.nVar+2*alpha.nVar+2*pnVar+4;
+%Bis hier!
 %     case 'modhopf'
 %         vars.omega=VariableVector(zeros(1,1), offset+x.nVar+alpha.nVar,{'omega'});
 %         vars.w1=VariableVector(ones(x.nVar,1), offset+x.nVar+alpha.nVar+1,{'w1'});
@@ -95,17 +106,17 @@ switch type
 % modifizieren, um auch hier das p mit zu berücksichtigen
 
         case 'modhopf'
-        vars.omega=VariableVector(zeros(1,1), offset+x.nVar+alpha.nVar+p.nVar,{'omega'});
-        vars.w1=VariableVector(ones(x.nVar,1), offset+x.nVar+alpha.nVar+1+p.nVar,{'w1'});
-        vars.w2=VariableVector(zeros(x.nVar,1), offset+2*x.nVar+alpha.nVar+1+p.nVar,{'w2'});
-        vars.v1=VariableVector(zeros(x.nVar,1), offset+3*x.nVar+alpha.nVar+1+p.nVar,{'v1'});
-        vars.v2=VariableVector(zeros(x.nVar,1), offset+4*x.nVar+alpha.nVar+1+p.nVar,{'v2'});
-        vars.g1=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+1+p.nVar,{'g1'});
-        vars.g2=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+2+p.nVar,{'g2'});
-        vars.u=VariableVector(zeros(x.nVar,1), offset+5*x.nVar+alpha.nVar+3+p.nVar,{'u'});
-        vars.r=VariableVector(zeros(alpha.nVar,1),offset+6*x.nVar+alpha.nVar+3+p.nVar,{'r'});
-        vars.l=VariableVector(zeros(1,1),offset+6*x.nVar+2*alpha.nVar+3+p.nVar,{'l'});
-        vars.nVar=6*x.nVar+2*alpha.nVar+4+p.nVar;
+        vars.omega=VariableVector(zeros(1,1), offset+x.nVar+alpha.nVar+pnVar,{'omega'});
+        vars.w1=VariableVector(ones(x.nVar,1), offset+x.nVar+alpha.nVar+1+pnVar,{'w1'});
+        vars.w2=VariableVector(zeros(x.nVar,1), offset+2*x.nVar+alpha.nVar+1+pnVar,{'w2'});
+        vars.v1=VariableVector(zeros(x.nVar,1), offset+3*x.nVar+alpha.nVar+1+pnVar,{'v1'});
+        vars.v2=VariableVector(zeros(x.nVar,1), offset+4*x.nVar+alpha.nVar+1+pnVar,{'v2'});
+        vars.g1=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+1+pnVar,{'g1'});
+        vars.g2=VariableVector(zeros(1,1), offset+5*x.nVar+alpha.nVar+2+pnVar,{'g2'});
+        vars.u=VariableVector(zeros(x.nVar,1), offset+5*x.nVar+alpha.nVar+3+pnVar,{'u'});
+        vars.r=VariableVector(zeros(alpha.nVar,1),offset+6*x.nVar+alpha.nVar+3+pnVar,{'r'});
+        vars.l=VariableVector(zeros(1,1),offset+6*x.nVar+2*alpha.nVar+3+pnVar,{'l'});
+        vars.nVar=6*x.nVar+2*alpha.nVar+4+pnVar;
     otherwise
         error('unknown type to generate variable list')
 end
