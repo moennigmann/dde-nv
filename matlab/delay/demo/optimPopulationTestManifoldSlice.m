a6efd895190927c7a1d6f7738ac61d8e10f64dac
Jonas Otten <jonas.otten@rub.de>
2018-10-24 17:50:51 +0200

    included compiling section

--- matlab/delay/demo/optimPopulationTestManifoldSlice.m
+++ matlab/delay/demo/optimPopulationTestManifoldSlice.m
@@ -1,3 +1,8 @@
+%% Demo of DDE-NV
+% The following script demonstrates the basic steps to run a steady state
+% optimization of a delayed system with robust exponential stability
+% constraints using the DDE-NV toolbox
+
 
 %% prepare matlab
 close all
@@ -11,18 +16,20 @@ g=2;
 param=[a;g];
 xGue=[1;1];
 
-%% name states and parameters
+% name states and parameters
 xNom=VariableVector(xGue,0,[{'juvenile'};{'mature'}]);
 alphaNom=VariableVector(param,2,[{'a'};{'g'}]);
 p=VariableVector([],4,[]);
 
-%% define system dynamics
+% define state dependent delay
 tau=@(x,alpha,p)1.5-0.5*exp(-x(2));
+
+% define system dynamics
 popModel=@(x,xtau,alpha,p)[alpha(1)*x(2)-alpha(2)*x(1)-alpha(1)*exp(-alpha(2)*tau(x,alpha,p))*xtau(2,1);
     alpha(1)*exp(-alpha(2)*tau(x,alpha,p))*xtau(2,1)-x(2)^2];
 
 
-%% collect them in an object 
+% collect them in an object 
 popDDE=DDE(@(x,xtau,alpha,p)popModel(x,xtau,alpha,p)',tau,xNom,alphaNom,p);
 
 
@@ -35,24 +42,35 @@ clear a
 clear gmaxStepLength
 
 %% construct object ''aDDENLP''
+% combine system dynamics, cost functions, box contraints etc. to form an
+% nonlinear programm
 
 J=@(x)x(1)/(x(1)+x(2));
 
 aDDENLP=DDENLP(J,popDDE,xNom,[0;0],[Inf;Inf],[0;0],[10;4],-Inf(0,1),Inf(0,1));
+
 clear popDDE
 clear J
 clear xNom
 
+% define numerical options
 aDDENLP.optionsInitEqCons=optimoptions('fsolve','Algorithm','levenberg-marquardt','MaxIter',10000,'MaxFunEvals',200000,'display','off','TolFun',1e-9,'TolX',1e-9);
 aDDENLP.optionsInitOptim=optimoptions('fmincon','Algorithm','active-set','MaxIter',10000,'MaxFunEvals',200000,'display','off');
 aDDENLP.optionsMainOptim=optimoptions('fmincon','Algorithm','active-set','MaxIter',10000,'MaxFunEvals',200000,'display','iter','TolFun',1e-9,'TolX',1e-9,'TolCon',1e-6,'RelLineSrchBnd',1e-5);
 
+aDDENLP.maxAllowedRealPart=-0.1;
 
 %% initialize steady state constraints
 aDDENLP.initializeStSt();
 
-%% add NV Cons
+%% add normal vector constraints
+% these are responsible for the robust exponential stability
 
+% compile c-code describing the constraints
+mex populationModelModFoldMani.c
+mex populationModelModFoldNV.c
+
+% normal vector constraint for first exponential stability boundary
 alphaCrit1=VariableVector([5;0.1],0,[{'acrit1'};{'gcrit1'}]);
 xCrit1=VariableVector([29.87;4.3],0,{'xcrit1'});
 
@@ -60,6 +78,8 @@ aDDENLP.addNVCon('modfold',@populationModelModFoldMani,@populationModelModFoldNV
 clear alphaCrit1
 clear xCrit1
 
+
+% normal vector constraint for second exponential stability boundary
 alphaCrit2=VariableVector([4.6287;3.7225],0,[{'acrit1'};{'gcrit1'}]);
 xCrit2=VariableVector([0.1144;0.0939],0,{'xcrit1'});
 
@@ -70,11 +90,10 @@ clear xCrit2
 
 %% initatialize constraints and prepare optimization
 aDDENLP.initNVCons();
+aDDENLP.concatConstraints();
+aDDENLP.concatInitPoints();
 
-
-%% show manifolds
-
-%%
+%% visualize the  manifolds
 
 manifoldSlices(1)=ManifoldSlice(aDDENLP.NVCon(1),[1 2]);
 manifoldSlices(2)=ManifoldSlice(aDDENLP.NVCon(2),[1 2]);
@@ -94,18 +113,18 @@ plot(aDDENLP.vars.critical(1).alpha.values(1),aDDENLP.vars.critical(1).alpha.val
 plot(aDDENLP.vars.critical(2).alpha.values(1),aDDENLP.vars.critical(2).alpha.values(2),'kx');
 plot(manifoldSlices)
 
-aDDENLP.concatConstraints();
-aDDENLP.concatInitPoints();
 
 %% run optimization
-aDDENLP.maxAllowedRealPart=-0.1;
 
 aDDENLP.runOptim();
 aDDENLP.deconstructOptimum();
+
+% plot optimal nominal point
 plot(aDDENLP.vars.nominal.alpha.values(1),aDDENLP.vars.nominal.alpha.values(2),'ko');
 
 
 %% run simulation
+% simulation of system dynamics with optimal parameters
 
 point=aDDENLP.vars.nominal(end);
 
@@ -114,10 +133,10 @@ options=ddeset();
 sol=ddesd(aDDENLP,point,history,[0 30],options);
 
 figure(2);clf;
-% plot(sol.x,sol.y);
 plot(sol.x,[sol.y(1,:)/point.x.values(1);sol.y(2,:)/point.x.values(2)])
 
 %% check stability
+% calculate eigenvalues at special points of interest
 
 [~]=aDDENLP.checkStabilityPoint('nominal');
 [~]=aDDENLP.checkStabilityPoint('critical');
